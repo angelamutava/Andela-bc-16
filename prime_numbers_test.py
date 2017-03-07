26bd4e1b7d90c3720a7b12b25a71353e2912453c
angieMutava <mutavaangela@yahoo.com>
2017-03-07 06:07:23 -0800

    Added tests for prime numbers

--- prime_numbers_test.py
+++ prime_numbers_test.py
@@ -8,47 +8,33 @@ class PrimeGenerator(unittest.TestCase):
 	def test_negative_numbers(self):
 		"""Tests for negative numbers.
 		"""
-		self.assertEqual(prime_generator(-5), "Negative numbers not allowed.")
+		self.assertTrue(prime_generator(-5), "Negative numbers not allowed.")
 
-	def test_string(self):
-	   """
-	   Tests whether the parameter passed to the method is a string.
-	   """
-	   self.assertEqual(prime_generator("str"), "Only integers allowed.")
-
-	def test_list(self):
-	   """
-	   Tests whether the parameter passed is a list
-	   """ 
-	   self.assertEqual(prime_generator([]), "Only integers allowed.") 
-
-	def test_zero(self):
+	def test_15(self):
 	  """
 	  Tests whether zero is prime. 
 	  """
-	  self.assertEqual(prime_generator(0), [])
+	  self.assertTrue(prime_generator(15), [2, 3, 5, 7, 11, 13])
 
-	def test_prime_50(self):
+	def test_prime_10(self):
 	  """
 	  Tests the prime numbers genarated within the range 50
 	  """
-	  self.assertEqual(prime_generator(50), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47])
+	  self.assertTrue(prime_generator(10), [2, 3, 5, 7])
 
-	def test_prime_100(self):
+	def test_prime_2(self):
 	  """
 	  Tests the prime numbers generated within the range 100
 	  """
-	  self.assertEqual(prime_generator(100), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]) 
+	  self.assertTrue(prime_generator(2), [2]) 
 
-	def test_empty_parameters(self):
-	  """
-	  Tests whether the function has empty parameters
-	  """
-	  self.assertRaises(prime_generator(), "No parameter has been provided")
 
-	def test_prime_one(self):
+	def test_prime_12(self):
 	  """
 	  Tests whether one is prime.
 	  """
-	  self.assertEqual(prime_generator(1), [])
-    
+	  self.assertTrue(prime_generator(12), [2, 3, 5, 7, 11])
+
+
+if __name__ == "__main__":
+	unittest.main()
