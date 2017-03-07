492890fee03eddcdf4bef8dd9e22f865374d273f
angieMutava <mutavaangela@yahoo.com>
2017-03-07 05:36:30 -0800

    Added prime number generator function

---
+++ prime_numbers.py
@@ -0,0 +1,26 @@
+import math
+def is_prime(number):
+    """This is a function that checks whether the number passed is prime."""
+    if number <= 0:
+        return False
+    elif number == 1:
+        return False
+    elif number == 2:
+        return True
+    else:
+        for i in range(2, int(math.sqrt(number))):
+            if number % i == 0:
+                return False
+        return True
+
+def prime_generator(number):
+    """It generates prime numbers in the given range of number."""
+    if number < 0:
+        return "Negative numbers not allowed."
+    if type(number) != int:
+        return "Only integers allowed."
+    result = []
+    for i in range(2, number + 1):
+        if is_prime(i):
+            result.append(i)
+    return result
